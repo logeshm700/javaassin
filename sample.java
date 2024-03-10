a37cc030fcc89e85a13c38905db407d9278df931
logeshm700 <117431024+logeshm700@users.noreply.github.com>
2024-03-10 21:59:22 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,31 +1,21 @@
 // Project.java
-@Entity
 public class Project {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    
+    
     private Long id;
-
-    @NotBlank
     private String name;
-
     private String description;
-
-    @NotNull
     private LocalDate startDate;
-
     private LocalDate endDate;
 
-    // Getters and setters
+
 }
 
-// ProjectRepository.java
-public interface ProjectRepository extends JpaRepository<Project, Long> {
+public interface ProjectRepository extends JpaRepository<Project, Long>
+    {
 }
 
-// ProjectService.java
-@Service
 public class ProjectService {
-    @Autowired
     private ProjectRepository projectRepository;
 
     public List<Project> getAllProjects() {
@@ -57,34 +47,27 @@ public class ProjectService {
     }
 }
 
-// ProjectController.java
-@RestController
-@RequestMapping("/projects")
+
 public class ProjectController {
-    @Autowired
+   
     private ProjectService projectService;
 
-    @GetMapping
     public List<Project> getAllProjects() {
         return projectService.getAllProjects();
     }
 
-    @GetMapping("/{id}")
     public Project getProjectById(@PathVariable Long id) {
         return projectService.getProjectById(id);
     }
 
-    @PostMapping
     public Project createProject(@RequestBody Project project) {
         return projectService.createProject(project);
     }
 
-    @PutMapping("/{id}")
     public Project updateProject(@PathVariable Long id, @RequestBody Project updatedProject) {
         return projectService.updateProject(id, updatedProject);
     }
 
-    @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteProject(@PathVariable Long id) {
         boolean deleted = projectService.deleteProject(id);
         return deleted ? ResponseEntity.ok("Project deleted") : ResponseEntity.notFound().build();
